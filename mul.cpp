11f82aade9b7fcae3d86976e43cffa1e4971bf7b
hautran_itr_intern <hauken146@gmail.com>
2023-04-04 22:40:23 +0700

    update: using steam in & stream out

--- mul.cpp
+++ mul.cpp
@@ -1,32 +1,37 @@
 #include "HLS/hls.h"
 #include <stdio.h>
+#include <stdlib.h> 
 
-component int conv1(int top1[3], int top2[3], int top3[3])
+component void conv1(ihc::stream_in<int>  &a,
+                    ihc::stream_in<int>  &b,
+			              ihc::stream_out<int> &c)
 {
-  int i;
-  for (i = 0; i < 6; i++)
+  int temp1,temp2,temp3;
+  for (int i = 0; i < 6; i++)
   {
-    top3[i] = top1[i]  * top2[i];
+    temp1 = a.read();
+    temp2=  b.read();
+    temp3 = temp1*temp2;
+    
   }
-  return top3[i];
+  c.write(temp3);
 }
 
 int main(void)
 {
+  ihc::stream_in<int> input_stream;
+  ihc::stream_in<int> input_stream2;
+  ihc::stream_out<int> output_stream3;
   printf("start");
-  int top1[3];
-  int top2[3];
-  int top3[3];
-  int t4 = 1;
   for (int j = 0; j < 3; j++)
   {
-    top1[j] = j;
+    input_stream.write(j);
   }
   for (int i = 3; i < 6; i++)
   {
-    top2[i - 3] = i;
+    input_stream2.write(i);
   }
-  t4 = conv1(top1, top2, top3);
+  conv1(input_stream, input_stream2, output_stream3);
 
   printf("done");
   return 0;

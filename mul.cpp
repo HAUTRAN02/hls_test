c7fd1421f2d8e36adb22b5d493130b4ade8bd049
hautran_itr_intern <hauken146@gmail.com>
2023-04-04 16:24:45 +0700

    update: covert array

--- mul.cpp
+++ mul.cpp
@@ -1,7 +1,7 @@
 #include "HLS/hls.h"
 #include <stdio.h>
 
-component void conv1(float in[6][6], float kernel[6][1][1], float out[6][6][6]){
+component void conv1(float in[36], float kernel[6], float out[36*6]){
  int channel, row, col; 
  int i,j; 
  for(channel=0;channel<6;channel++){
@@ -9,7 +9,7 @@ component void conv1(float in[6][6], float kernel[6][1][1], float out[6][6][6]){
      for(col=0;col<6;col++){
        for(i=0;i<1;i++){
          for(j=0;j<1;j++){
-              out[channel][row][col] += in[row+i][col+j] * kernel[channel][i][j];
+              out[6*6*channel + 6*row + col] += in[(row+i)*6 + (col+j)] * kernel[6*6*channel + 6*row + col];
          }
        }
      }
@@ -18,17 +18,17 @@ component void conv1(float in[6][6], float kernel[6][1][1], float out[6][6][6]){
 }
 
 int main (void) {
-float in[6][6];
-float kernel[6][1][1];
-float out[6][6][6];
+float in[36];
+float kernel[6];
+float out[36*6];
 for(int i=0;i<6;i++)
     for(int j=0;j<6;j++)
         {
-            in[i][j]=1.2;
+            in[6*i+j]=1.2;
         }
 for(int i=0;i<6;i++)
 {
-kernel[i][0][0] = 1.1;
+kernel[i] = 1.1;
 }
 conv1(in,kernel,out);
 

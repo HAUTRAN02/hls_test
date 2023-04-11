580dd5c7bf7869f3f766207fc92b250f22172877
hautran_itr_intern <hauken146@gmail.com>
2023-04-11 20:55:01 +0700

    update: add avpooling1 function

--- lenet5/lib5.cpp
+++ lenet5/lib5.cpp
@@ -44,6 +44,16 @@ void relu1(float in2[6][28][28], float out2[6][28][28]){
        }
    }
 }
+void avgpooling1(float in3[6][28][28], float out3[6][14][14]){
+   int n_channel, i, j;
+   for(n_channel=0;n_channel<6;n_channel++){
+       for(i=0;i<28;i+=2){
+           for(j=0;j<28;j+=2){
+               out3[n_channel][i/2][j/2] = (in3[n_channel][i][j] + in3[n_channel][i+1][j] + in3[n_channel][i][j+1] + in3[n_channel][i+1][j+1])/ (4.0f);
+           }
+       }
+   }
+}
 component void pred(ihc::stream_in<float> &img_stream,
                     ihc::stream_in<float> &w1_stream,
                     ihc::stream_in<float> &b1_stream)
@@ -54,6 +64,7 @@ component void pred(ihc::stream_in<float> &img_stream,
   hls_register float o1_matrix[6][28][28];
 
   hls_register float o2_matrix[6][28][28];
+  hls_register float o3_matrix[6][14][14];
 
 #ifdef USE_COALESCE
 #pragma loop_coalesce
@@ -83,6 +94,7 @@ component void pred(ihc::stream_in<float> &img_stream,
 
   conv1(img_matrix, w1_matrix, b1_matrix, o1_matrix);
   relu1(o1_matrix,o2_matrix);
+  avgpooling1(o2_matrix,o3_matrix);
 // #ifdef USE_COALESCE
 // #pragma loop_coalesce
 // #endif
